09cd1e7f68a404cb293a1a6347b77ad51be243fc
Pete LePage <pete.lepage@pobox.com>
2023-01-06 09:55:40 -0500

    ribbon class name fix

--- src/components/ExperimentalRibbon.js
+++ src/components/ExperimentalRibbon.js
@@ -3,6 +3,6 @@ import './ExperimentalRibbon.css';
 
 export default function ExperimentalRibbon(props) {
   return (
-    <a class="github-fork-ribbon left-top" href="/about" data-ribbon="Experimental" title="Experimental">P</a>
+    <a className="github-fork-ribbon left-top" href="/about" data-ribbon="Experimental" title="Experimental">P</a>
   );
 }
