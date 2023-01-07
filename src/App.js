baa68891e45259bd21e126000226124d57cdde6c
petele <pete.lepage@pobox.com>
2023-01-06 19:18:33 -0500

    Add necessary fonts

--- src/App.js
+++ src/App.js
@@ -24,6 +24,10 @@ import Forgot from './views/Forgot';
 import Profile from './views/Profile';
 
 import './App.css';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 export default function App() {
 
