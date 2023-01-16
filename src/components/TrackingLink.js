db9340effcd96de25a9170cb846882297515a473
petele <pete.lepage@pobox.com>
2023-01-16 11:54:07 -0500

    Fixes #15 - TrackingLink to function

--- src/components/TrackingLink.js
+++ src/components/TrackingLink.js
@@ -6,39 +6,34 @@ import {
 
 import getTrackingURL from '../helpers/getTrackingURL';
 
-class TrackingLink extends React.Component {
-
-  getLabel(trackingNumber) {
-    if (typeof trackingNumber !== 'string') {
-      return null;
-    }
-    if (this.props.width === 'lg') {
-      return trackingNumber;
-    }
-    if (trackingNumber.length < 17) {
-      return trackingNumber;
-    }
-    const len = trackingNumber.length;
-    const start = trackingNumber.substring(0, 2);
-    const end = trackingNumber.substring(len - 6);
-    return `${start}...${end}`;
-  }
-
-  render() {
-    const shipper = this.props.row.shipper;
-    const trackingNumber = this.props.row.trackingNumber;
-    const url = getTrackingURL(shipper, trackingNumber);
-    const label = this.getLabel(trackingNumber);
-
-    if (url && label) {
-      return (
-        <Link href={url} target="_blank" rel="noreferrer">
-          {label}
-        </Link>
-      );
-    }
+function _getLabel(trackingNumber, width) {
+  if (typeof trackingNumber !== 'string') {
     return null;
   }
+  if (width === 'lg') {
+    return trackingNumber;
+  }
+  if (trackingNumber.length < 17) {
+    return trackingNumber;
+  }
+  const len = trackingNumber.length;
+  const start = trackingNumber.substring(0, 2);
+  const end = trackingNumber.substring(len - 6);
+  return `${start}...${end}`;
 }
 
-export default TrackingLink;
+export default function TrackingLink({row, width}) {
+  const shipper = row.shipper;
+  const trackingNumber = row.trackingNumber;
+  const url = getTrackingURL(shipper, trackingNumber);
+  const label = _getLabel(trackingNumber, width);
+
+  if (url && label) {
+    return (
+      <Link href={url} target="_blank" rel="noreferrer">
+        {label}
+      </Link>
+    );
+  }
+  return null;
+}
