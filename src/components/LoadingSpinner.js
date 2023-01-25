d7ad42591d20292e67261fda689ce10e7dafec21
petele <pete.lepage@pobox.com>
2023-01-24 19:35:22 -0500

    Fixes #47 - offer reload on loading spinner

--- src/components/LoadingSpinner.js
+++ src/components/LoadingSpinner.js
@@ -1,9 +1,32 @@
+import * as React from 'react';
+
 import {
   Box,
+  Link,
   CircularProgress
 } from '@mui/material';
+import { gaEvent } from '../helpers/gaHelper';
 
 export default function LoadingSpinner() {
+  const [offerReload, setOfferReload] = React.useState(false);
+
+  React.useEffect(() => {
+    const timer = setTimeout(() => {
+      const url = window.location.pathname;
+      gaEvent('long_load', {path: url});
+      setOfferReload(true);
+    }, 5000);
+    return () => clearTimeout(timer);
+  }, []);
+
+  function clickReload() {
+    window.location.reload();
+  }
+
+  const pointerStyle = {
+    cursor: 'pointer !important',
+  };
+
   return (
     <Box sx={{
       marginTop: 8,
@@ -12,6 +35,12 @@ export default function LoadingSpinner() {
       alignItems: 'center',
     }}>
       <CircularProgress />
+      {offerReload && (
+        <Box sx={{mt: 4}}>
+          Sorry, this it taking longer than usual, want
+          to <Link onClick={clickReload} to="#" sx={pointerStyle}>reload</Link>?
+        </Box>
+      )}
     </Box>
   );
 }
