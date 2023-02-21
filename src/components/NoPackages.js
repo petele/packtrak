3b01ec57a4d8daa9699df366ebad9edc6950abe2
petele <pete.lepage@pobox.com>
2023-02-21 09:19:19 -0500

    punctuation fix

--- src/components/NoPackages.js
+++ src/components/NoPackages.js
@@ -2,6 +2,6 @@ import SadPanda from './SadPanda';
 
 export default function NoPackages() {
   return (
-    <SadPanda reason="you haven't added any packages yet." />
+    <SadPanda reason="you haven't added any packages yet" />
   );
 }
